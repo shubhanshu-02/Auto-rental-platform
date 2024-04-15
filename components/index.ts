08741779a99ccb1959e40a268c44c58ed939d43f
Shubhanshu <shubhanshu004@gmail.com>
2024-04-16 01:15:23 +0530

    update all index.ts components

--- components/index.ts
+++ components/index.ts
@@ -1,6 +1,11 @@
 import Hero from "./Hero"
 import CustomButton from "./CustomButton"
+import Navbar from "@/components/Navbar";
+import Footer from "@/components/Footer";
+
 export {
     Hero,
-    CustomButton
+    CustomButton,
+    Navbar,
+    Footer
 }
