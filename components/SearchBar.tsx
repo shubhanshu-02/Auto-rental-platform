f70925e0265acedb34b969384b3a83dade93367a
Shubhanshu <shubhanshu004@gmail.com>
2024-04-20 21:23:10 +0530

    new components

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -1,15 +1,20 @@
 "use client"
 
+import { useState } from "react"
 import SearchManufacturer from "./SearchManufacturer"
-
 const SearchBar = () => {
+
+const [manufacturer,setManufacturer] = useState("");
+
 const handleSearch = () => {}
 
     return (
     <div>
         <form className="searchbar" onSubmit={handleSearch}>
             <div className="searchbar__item ">
-                <SearchManufacturer />
+                <SearchManufacturer 
+                manufacturer = {manufacturer}
+                setManufacturer = {setManufacturer}/>
             </div>
         </form>
     </div>
--- components/index.ts
+++ components/index.ts
@@ -2,10 +2,16 @@ import Hero from "./Hero"
 import CustomButton from "./CustomButton"
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
+import SearchBar from "@/components/SearchBar";
+import CustomFilter from "@/components/CustomFilter";
+import SearchManufacturer from "@/components/SearchManufacturer";
 
 export {
     Hero,
     CustomButton,
     Navbar,
-    Footer
+    Footer,
+    SearchBar,
+    CustomFilter,
+    SearchManufacturer
 }
