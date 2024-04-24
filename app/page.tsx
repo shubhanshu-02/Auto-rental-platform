4c4abaa1c0ec6684687d8bd8b8f2476c4d25b061
Shubhanshu <shubhanshu004@gmail.com>
2024-04-25 02:52:28 +0530

    update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,34 +1,46 @@
-import { CustomFilter, Hero, SearchBar } from "@/components";
+import { CarCard, CustomFilter, Hero, SearchBar } from "@/components";
 import { getCars } from "@/utility";
 import Image from "next/image";
 
 export default async function Home() {
   const cars = await getCars();
-const isDataEmpty = !Array.isArray(cars) || cars.length < 1 || !cars;
+  const isDataEmpty = !Array.isArray(cars) || cars.length < 1 || !cars;
 
-  // console.log(cars)
+  console.log(cars)
 
   return (
     <main className="overflow-hidden">
       <Hero />
-       <div className="mt-12 padding-x padding-y max-width" id = "discover">
+      <div className="mt-12 padding-x padding-y max-width" id="discover">
         <div className="home__text-container">
           <h1 className="text-4xl font-extrabold"> Car Catalogue</h1>
           <p>explore the cars you might like</p>
         </div>
 
-<div className="home__filters">
-  <SearchBar />
-</div>
+        <div className="home__filters">
+          <SearchBar />
+        </div>
 
-<div className="home__filter-container">
-  {/* <CustomFilter title = "fuel" /> 
+        <div className="home__filter-container">
+          {/* <CustomFilter title = "fuel" /> 
   <CustomFilter title = "fuel" />  */}
 
-</div>
-{/* (!isDataEmpty ) */}
+        </div>
 
-       </div>
+        {!isDataEmpty ? (
+        <section>
+          {cars?.map((car) => (
+            <CarCard car={car} />
+          ))}
+        </section>
+        ) : (
+        <div className="home__error-container">
+          <h2 className="text-black text-xl font-bold"> Oops, no Cars</h2>
+          <p>{cars?.message}</p>
+        </div>
+        )}
+
+      </div>
     </main>
   )
 }
