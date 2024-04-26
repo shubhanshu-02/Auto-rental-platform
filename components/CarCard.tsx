50f18efb630a376bbc8fff1bdbfd63b3c5fdd1e5
Shubhanshu <shubhanshu004@gmail.com>
2024-04-26 23:09:58 +0530

    car card

--- components/CarCard.tsx
+++ components/CarCard.tsx
@@ -6,9 +6,13 @@ interface CarCardProps{
     car:CarProps;
 }
 const CarCard = ( {car} : CarCardProps ) => {
+    const {city_mpg, year, make, model, transmission, drive} = car;
     return (
-        <div> 
-            CarCard
+        <div className="car-card group"> 
+        <div className="car-card__content">
+            <h2 className="car-card__content-title"> {make} {model} </h2>
+            {/*  CarCard */}
+        </div>
         </div>
     )
 }
