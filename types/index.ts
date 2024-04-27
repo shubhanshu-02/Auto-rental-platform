b81440889d6002f91726cd655da55ede9babfe59
Shubhanshu <shubhanshu004@gmail.com>
2024-04-27 16:47:08 +0530

    add new paramenters to CustomButton

--- types/index.ts
+++ types/index.ts
@@ -3,6 +3,9 @@ import { MouseEventHandler } from "react";
 export interface CustomButtonProps{
     title : string;
     containerStyles?: string;
+    textStyles?: string;
+    rightIcon?:string;
+    isDisabled?:boolean;
     handleClick?: MouseEventHandler<HTMLButtonElement>;
     btnType?: "button" | "submit";
 }
@@ -26,3 +29,5 @@ export interface CarProps {
     transmission: string;
     year: number
 }
+
+
