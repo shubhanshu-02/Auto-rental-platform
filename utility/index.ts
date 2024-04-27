e2d81d68450462b765371ff26c34784ae3dc6665
Shubhanshu <shubhanshu004@gmail.com>
2024-04-27 16:47:25 +0530

    new function to calculate rent

--- utility/index.ts
+++ utility/index.ts
@@ -1,21 +1,17 @@
-// const axios = require('axios');
-
-// const options = {
-//   method: 'GET',
-//   url: 'https://cars-by-api-ninjas.p.rapidapi.com/v1/cars',
-//   params: {model: 'corolla'},
-//   headers: {
-//     'X-RapidAPI-Key': '8958fcf7f8msh57cf40ab167d7afp15810ejsn2f29d6c613d4',
-//     'X-RapidAPI-Host': 'cars-by-api-ninjas.p.rapidapi.com'
-//   }
-// };
-
-// try {
-// 	const response = await axios.request(options);
-// 	console.log(response.data);
-// } catch (error) {
-// 	console.error(error);
-// } 
+export const calculateCarRent = (city_mpg: number, year: number) => {
+    const basePricePerDay = 50; // Base rental price per day in dollars
+    const mileageFactor = 0.1; // Additional rate per mile driven
+    const ageFactor = 0.05; // Additional rate per year of vehicle age
+  
+    // Calculate additional rate based on mileage and age
+    const mileageRate = city_mpg * mileageFactor;
+    const ageRate = (new Date().getFullYear() - year) * ageFactor;
+  
+    // Calculate total rental rate per day
+    const rentalRatePerDay = basePricePerDay + mileageRate + ageRate;
+  
+    return rentalRatePerDay.toFixed(0);
+  };
 
 
 export async function getCars(){
